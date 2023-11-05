955191dd5887096f8bc957daa45bcb62db6b4488
Yevsiukov Aleksandr <91425006+WhoMustNotBeNamed@users.noreply.github.com>
2023-11-05 16:41:14 +0300

    Update find_x.s

--- Program/find_x.s
+++ Program/find_x.s
@@ -4,7 +4,7 @@
 
 find_x:
 .data
-	eps: .float 0.00000001	# Переменная определяющая точность вычисления
+	eps: .float 0.00000001		# Переменная определяющая точность вычисления
 .text	
 	# Сохранение адреса возврата
 	addi 		sp, sp, -4
@@ -13,15 +13,15 @@ find_x:
 	fmv.s		ft1 fa1		# a
 	fmv.s		ft2 fa2		# a
 	
-	fun(fa1)				# f(a)
+	fun(fa1)			# f(a)
 	fmv.s		ft3 fa0		# Сохранили f(a) в ft3
-	fun(fa2)				# f(b)
+	fun(fa2)			# f(b)
 	fmv.s		ft4 fa0		# Сохранили f(b) в ft4
 	
 	# Проверка, что в интервале есть решение
 	fmul.s		fs0 ft3 ft4	# f(a)*f(b)
-	li			s1 0
-    fcvt.s.w	fs10 s1
+	li		s1 0
+    	fcvt.s.w	fs10 s1
 	fle.s		t6 fs0 fs10 
 	beqz 		t6 error	# if f(a)*f(b) > 0, то останавливаем программу
 	
@@ -34,7 +34,7 @@ find_x:
 	
 	loop:	fun(fa1)			# f(a)
 		fmv.s		ft3 fa0		# Сохранили f(a) в ft3
-		fun(fa2)				# f(b)
+		fun(fa2)			# f(b)
 		fmv.s		ft4 fa0		# Сохранили f(b) в ft4
 		fmv.s		ft7 ft5		# x0 = xn
 		
@@ -54,13 +54,13 @@ find_x:
 		
 		# Проверка для перехода к новой границе интервала
 		fmul.s		fs5 ft3 ft8	# f(a)*f(x)
-		li			s1 0
+		li		s1 0
     	fcvt.s.w	fs10 s1
 		flt.s		t6 fs5 fs10 
 		bgtz 		t6 type1	# if f(a)*f(x) < 0
 	back1:		
 		fmul.s		fs5 ft4 ft8	# f(b)*f(x)
-		li			s1 0
+		li		s1 0
     	fcvt.s.w	fs10 s1
 		flt.s		t6 fs5 fs10
 		bgtz 		t6 type2	# if f(b)*f(x) < 0
@@ -69,34 +69,34 @@ find_x:
 		fmv.s		ft5 fs0
 		fsub.s		ft6 ft5 ft7	# x - x0
 		fmv.s		fa0 ft6
-		abs(fa0)				# |x-x0|
+		abs(fa0)			# |x-x0|
 		fmv.s		ft6 fa0
 		
 		# Проверка условия на выход из цикла
-    	la			t6 eps
-    	flw			fs9 (t6)
+    	la		t6 eps
+    	flw		fs9 (t6)
     	flt.s 		t6 fs9 ft6
-    	bnez		t6 loop     # if eps < |x-x0|, цикл продолжается
+    	bnez		t6 loop     	# if eps < |x-x0|, цикл продолжается
     		
     		
 	# Выход из подпрограммы
-	fmv.s			fa0 ft5
+	fmv.s		fa0 ft5
 	print_str("x = ")
 	print_float(fa0)
-	lw 				ra, (sp)
-	addi 			sp, sp, 4
+	lw 		ra, (sp)
+	addi 		sp, sp, 4
 	ret
 
 type1:
-	fmv.s		ft2 fs0	
+	fmv.s	ft2 fs0	
 	j	back1
 type2:
-	fmv.s		ft1 fs0			
+	fmv.s	ft1 fs0			
 	j	back2
 	
 error:
 	print_str("На интервале нет решения")
 	# Выход из подпрограммы
-	lw 			ra, (sp)
-	addi 		sp, sp, 4
+	lw 	ra, (sp)
+	addi 	sp, sp, 4
 	ret
